24688bbf70efddc5b3af3ae3926bbff1fbdec0c7
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-10-18 21:47:37 -0700

    my first pipe and alert guessing for lab 2 so I could understand properly

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use stict';
+
+let answer1 = prompt (' Does Paula enjoy nature? yes or no.');
+if (answer1 === 'yes'|| answer1 === 'y') {
+  console.log(' Heck yes, I love that you know that about me!');
+  alert('You got it dude!');
+} else if (answer1 === 'no'|| answer1 === 'n') {
+  console.log('Good guess, but that is heckin false. She be loving the nature.' );
+  alert('Good guess, but that is heckin false. She be loving the nature.');
+} else {
+  console.log('Well that is just invalid.');
+  alert('Participate with a yes or no, it is fun.');
+}
+
+//let answer2 = prompt('Is her favorite food fish?');
+//console.log(answer2);
+
+//let answer3 = prompt ('Was she born in Costa Rica?');
+//console.log(answer3);
+
+//let answer4 = prompt ('Does Paula spend time in saunas?');
+//console.log(answer4);
+
+//let answer5 = prompt ('Can she pick the best pineapple of all the grocery lands?');
+//console.log(answer5);
+
+
+// If Statements
