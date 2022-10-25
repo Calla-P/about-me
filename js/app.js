07c8fc36db940218aaaf021f3cb14de4f1c8394e
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-10-24 22:29:56 -0700

    I added for loops and made a score keeper plus adding some css to the thang

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use stict';
 
 let score = 0;
-/* let userName = prompt(' What is your name? ');
+let userName = prompt(' What is your name? ');
 console.log(userName);
 
 alert(' Hello and Welcome ' + userName);
@@ -10,7 +10,7 @@ answer1 = answer1.toLowerCase();
 if (answer1 === 'yes' || answer1 === 'y') {
   //console.log('You got it dude!');
   alert('Heck yes, I love that you know that about me!');
-  score++
+  score++;
 } else if (answer1 === 'no' || answer1 === 'n') {
   //console.log('Good guess, but that is heckin false. She be loving the nature.' );
   alert('Good guess, but that is heckin false. She be loving the nature.');
@@ -27,7 +27,7 @@ if (answer2 === 'yes' || answer2 === 'y') {
 } else if (answer2 === 'no' || answer2 === 'n') {
   //console.log(' Correcto Mundo ');
   alert('You know Paula well, wowzers!');
-  score++
+  score++;
 } else {
   //console.log('Why they no participate?');
   alert('Please try a yes or a no to get special message.');
@@ -38,6 +38,7 @@ answer3 = answer3.toLowerCase();
 if (answer3 === 'yes' || answer3 === 'y') {
   //console.log('Correct');
   alert('You are incredible and correct! Great Job.');
+  score++;
 } else if (answer3 === 'no' || answer3 === 'n') {
   //console.log('Incorrecto!');
   alert('That is incorrect and you are rad for guessing!');
@@ -51,6 +52,7 @@ answer4 = answer4.toLowerCase();
 if (answer4 === 'yes' || answer4 === 'y') {
   //console.log('Typed yes');
   alert(' You are on to her, Three times a week, my friend.');
+  score++;
 } else if (answer4 === 'no' || answer4 === 'n') {
   //console.log('Is wrong for that.lol');
   alert('If no meant yes you would be right but you are wrong and that is fine.');
@@ -63,14 +65,15 @@ let answer5 = prompt('Can she pick the best pineapple of all the grocery lands?'
 answer5 = answer5.toLowerCase();
 if (answer5 === 'yes' || answer5 === 'y') {
   //console.log('They agree');
-  alert('Absolutely ' + userName + ' and with confidence. You ARE correct and awesome! ' + userName);
+  alert('Absolutely ' + userName + ', and with confidence. You ARE correct and awesome! ' );
+  score++;
 } else if (answer5 === 'no' || answer5 === 'n') {
   //console.log('said no');
   alert(userName + ' That is not right, she picks those pineapples nicely. You are kind for playing regardless of being wrong. lol ');
 } else {
   //console.log(' invalid response.');
   alert('Bold statement ' + userName + ' but a y/n will get us where we need to go.');
-} */
+}
 
 function guessANumber() {
 
@@ -80,20 +83,26 @@ function guessANumber() {
   while (attempt < 5) {
     let answer1 = prompt('Guess a number between 1-10');
     if (answer1 > 5) {
+      //console.log('corret');
       alert(' That is too high. Give it another go!');
       attempt++;
     } else if (answer1 < 5) {
+      //console.log('incorret');
       alert('That is too low. ');
       attempt++;
     }
     else {
+      //console.log('invalid');
       attempt = 6;
       break;
     }
   }
   if (attempt === 6) {
+    //console.log('corret');
     alert(' You got it right! It is 5!');
+    score++;
   } else {
+    //console.log('incorret');
     alert(' The correct answer was 5.');
   }
 }
@@ -106,7 +115,7 @@ function questionSeven() {
   let attempts = 0;
   while (attempts <= 6) {
     let answer7 = prompt(
-      ' Hello user. Which of the following states have I lived in? (guess one): Alabama, Idaho, Flordia, Montana, California, Mississippi, Colorado, New Jersey '
+      userName + ', You have come a long way. Thank you. Now take a sip of water and answer the last question. Which of the following states have I lived in? (guess one): Alabama, Idaho, Flordia, Montana, California, Mississippi, Colorado, New Jersey '
     );
     let stateEye = answer7.toLowerCase();
     for (let i = 0; i <= statesIlived.length; i++) {
@@ -123,25 +132,4 @@ function questionSeven() {
 }
 questionSeven();
 
-/* let answer8 = false;
-
-for (let i = 0; i < statesIlived.length; i++) {
-  if (answer7 === statesIlived[i]) {
-    answer8 = true;
-  }
-}
-
-if (answer8) {
-  alert(' Correct. ');
-} else {
-  alert('Incorrect. ');
-}
-
-for (let i = 0; i < 7; i++) {
-  console.log(' Atempted 6 times I see. ');
-}
-
-console.log(statesIlived[6]);
- */
-
-//alert('You got ' + score + 'right.' );
+alert('Out of 7, You got ' + score + ' right. That is rad!' );
